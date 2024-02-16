8c74e046323b9b73d38b3630aec2987e86be02f1
gusnl88 <gusnl88@naver.com>
2024-02-16 21:34:54 +0900

    userList finAll and delete

--- 17_sequelize/controller/Cuser.js
+++ 17_sequelize/controller/Cuser.js
@@ -1,4 +1,4 @@
-const models=require('../models')
+const models = require("../models");
 
 exports.getSignin = (req, res) => {
   res.render("user/signin");
@@ -8,54 +8,61 @@ exports.getSignup = (req, res) => {
   res.render("user/signup");
 };
 
+exports.findAll = (req, res) => {
+  models.User.findAll().then((result) => {
+    res.render("user/userList", { userList: result });
+  });
+};
 
-
-
-
-exports.postSignin=(req,res)=>{
-    models.User.findOne({
-      where: { userid: req.body.userid,pw:req.body.pw },
-    }).then((result) => {
-      console.log(result);
-      res.send(result)
-    });
+exports.postSignin = (req, res) => {
+  models.User.findOne({
+    where: { userid: req.body.userid, pw: req.body.pw },
+  }).then((result) => {
+    console.log(result);
+    res.send(result);
+  });
 };
 
 exports.postSignup = (req, res) => {
   console.log(req.body);
   models.User.create({
-    userid:req.body.userid,
-    pw:req.body.pw,
-    name:req.body.name,
-  }).then((result)=>{
+    userid: req.body.userid,
+    pw: req.body.pw,
+    name: req.body.name,
+  }).then((result) => {
     res.end();
-  })
+  });
 };
 
 exports.postProfile = (req, res) => {
-    models.User.findOne({
-      where: { userid: req.body.userid },
-    }).then((result) => {
-      console.log(result);
-      res.render('user/profile',{user:result})
-    });
+  models.User.findOne({
+    where: { userid: req.body.userid },
+  }).then((result) => {
+    console.log(result);
+    res.render("user/profile", { user: result });
+  });
 };
 
-exports.postEdit=(req,res)=>{
-    models.User.update({
-        pw:req.body.pw,
-        name:req.body.name
-    },{
-        where:{id:req.body.id}
-    }).then((result)=>{
-        res.end()
-    })
-}
 
-exports.postDelete=(req,res)=>{
-    models.User.destroy({
-        where:{id:req.body.id}
-    }).then((result)=>{
-        res.end();
-    })
-}
+exports.postEdit = (req, res) => {
+  models.User.update(
+    {
+      pw: req.body.pw,
+      name: req.body.name,
+    },
+    {
+      where: { id: req.body.id },
+    }
+  ).then((result) => {
+    res.end();
+  });
+};
+
+exports.postDelete = (req, res) => {
+    console.log(req.body)
+  models.User.destroy({
+    where: { id: req.body.id },
+  }).then((result) => {
+    res.end();
+  });
+};
